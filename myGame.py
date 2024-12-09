e433cb054f1fd9b9e2744f84f9d18dbc3c7e5db9
M. Jabs <m.jabs@bbk.schule>
2024-12-09 08:12:29 +0100

    Funktionen in die Datei myGameMethods.py verschoben

--- myGame.py
+++ myGame.py
@@ -1,26 +1,8 @@
-import random
-import os
-
-def roll_dice():
-    return random.randint(1, 6)
+import myGameMethods
 
 def main():
-    welcomeMessage()
-    print(roll_dice())
-
-def welcomeMessage():
-    os.system('cls' if os.name=="nt" else 'clear')
-    print("Game 'Pig'")
-    print("=================")
-    print("Welcome to the push-your-luck game Pig!")
-    print("This is a die game for 2-4 players.")
-    print("The game is played over several rounds.")
-    print("In each round, players may roll a die.")
-    print("If a player rolls a 1, they score no points for the round and it becomes the next player's turn.")
-    print("If a player rolls a 2-6, they can either hold (stop rolling) or roll again.")
-    print("If they hold, they score the sum of the rolls during the round.")
-    print("The first player to score 100 or more points wins the game.")
-    print("Good luck!")
+    myGameMethods.welcomeMessage()
+    print(myGameMethods.roll_dice())
 
 if __name__ == '__main__':
-    print(random.randint(1, 6))
+    main()
